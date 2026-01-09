aa0c3f976dc8f44cfb23e523d18db479361f4965
Mehnaz Shafeek <mehnazshafeek@gmail.com>
2026-01-09 09:48:02 +0400

    Time: 2510 ms (48.31%) | Memory: 130.7 MB (51.08%) - LeetSync

--- 204-count-primes/count-primes.py
+++ 204-count-primes/count-primes.py
@@ -1,20 +1,22 @@
 class Solution(object):
     def countPrimes(self, n):
+        #Mehnaz
+        #Edge case
         if n < 2:
             return 0
+        #creating a boolean array first uptil n 
         isPrime = [True] * n
+        # marking 0 and 1 as NON PRIME
         isPrime[0] = isPrime[1] = False
-
+        # each number from 2 up to √n
         for i in range(2,int(n**0.5)+1):
             if isPrime[i]:
-                for j in range(i*i,n,i):
+                for j in range(i*i , n , i):
                     isPrime[j] = False
-                
+
+
         return sum(isPrime)
-            
+
+
         
-        """
-        :type n: int
-        :rtype: int
-        """
-        
+       
