87e5c8f59dfc2ca222d018bc1241f78ea03f6d76
Mehnaz Shafeek <mehnazshafeek@gmail.com>
2026-01-25 18:31:58 +0400

    Time: 7 ms (56.18%) | Memory: 26.1 MB (7.81%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -1,20 +1,17 @@
 class Solution(object):
     def rotate(self, nums, k):
-        n = len(nums)
-        r= k%n
+        n = len(nums) # 7
+        r = k%n # 3
         res = []
         for i in range(n-r,n):
             res.append(nums[i])
         for i in range(0,n-r):
             res.append(nums[i])
-        for i in range(n):
-            nums[i]= res[i]
+        for i in range(0,n):
+            nums[i] = res[i]
 
 
 
-        
-
-       
         
         """
         :type nums: List[int]
