dfddfdeb7b6064ced468421a71a666c7a2c2a6d9
Mehnaz Shafeek <mehnazshafeek@gmail.com>
2026-01-27 11:24:39 +0400

    Time: 8 ms (42.79%) | Memory: 13.6 MB (69.45%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -1,14 +1,16 @@
 class Solution(object):
     def moveZeroes(self, nums):
-      k = 0
-      for i in range(0,len(nums)):
-        if nums[i] == 0:
-            continue
-        
-        nums[k] , nums[i] = nums[i],nums[k]
-        k += 1
-        
+        k = 0
+        n = len(nums)
+        for i in range(0,n):
+            if nums[i] != 0:
+                nums[k] = nums[i]
+                k += 1
+        for i in range(k,n):
+            nums[i] = 0
 
+        return nums
+     
 
          
 
