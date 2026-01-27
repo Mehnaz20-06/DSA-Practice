3a7803aeec0daba709ea7ded3d9ae285d177ced3
Mehnaz Shafeek <mehnazshafeek@gmail.com>
2026-01-27 18:41:06 +0400

    Time: 31 ms (30.08%) | Memory: 18.3 MB (17.90%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -2,12 +2,22 @@ class Solution(object):
     def groupAnagrams(self, strs):
         res = defaultdict(list)
         for s in strs:
-            count = [0]*26
-            for c in s:
-                count[ord(c) - ord("a")] += 1
+            count = [0] * 26
+            for i in range(0,len(s)):
+                count[ord(s[i])  - ord("a")] += 1
+                
             res[tuple(count)].append(s)
         return res.values()
+
+
+
+
+
+
         
+
+
+     
         
         
         
